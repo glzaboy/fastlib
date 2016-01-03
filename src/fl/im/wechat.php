1f796d53a63ced5a44e91fe501cb7548a8d7c221
glzaboy <glzaboy@163.com>
2016-01-03 17:57:43 +0800

    modify weixin valid

--- src/fl/im/wechat.php
+++ src/fl/im/wechat.php
@@ -519,41 +519,6 @@ class wechat extends object
         }
     }
 
-    /**
-     * 和官方的公众平台进行对接
-     */
-    public function valid()
-    {
-        return self::checksignature();
-    }
-
-    /**
-     * 进行签名认证
-     *
-     * @return boolean
-     */
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-        $token = $this->token;
-        $tmpArr = array(
-            $token,
-            $timestamp,
-            $nonce
-        );
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode($tmpArr);
-        $tmpStr = sha1($tmpStr);
-        
-        if ($tmpStr == $signature) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     private function encryptMessage($message)
     {
         if ($this->EncodingAESKey) {
